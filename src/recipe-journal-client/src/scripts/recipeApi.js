5d7db879cd34ca71e59b7b50d6f52e430deadabf
sm_d3po <seanmarek@gmail.com>
2023-09-27 19:15:59 -0700

    got high made pi, now my hands are full

--- src/recipe-journal-client/src/scripts/recipeApi.js
+++ src/recipe-journal-client/src/scripts/recipeApi.js
@@ -1,5 +1,25 @@
 export default class RecipeApi {
-    async getRecipeList() { }
+    async getRecipeList() {
+        return [new RecipeListDto('German Apply Pancakes', 'fluffy cakes w/ apples', [
+            new RecipeComponentDto('','', [
+                new RecipeStepDto('', 'mix together eggs, flour, salt, milk.', [
+                    new IngredientDto('egg', 'count', 4),
+                    new IngredientDto('flour', 'cup', .75),
+                    new IngredientDto('salt', 'teaspoon', 1),
+                ]),
+                new RecipeStepDto('', 'melt butter into pans, place cut apples into pans, concentric circles usually work best', [
+                    new IngredientDto('butter', 'tablespoon', 4),
+                    new IngredientDto('apple', 'count', 2)
+                ]),
+                new RecipeStepDto('', 'mix together sugar and cinnamon', [
+                    new IngredientDto('sugar', 'cup', .33),
+                    new IngredientDto('flour', 'cup', .75),
+                ]), 
+                new RecipeStepDto('', 'pour mixture over apples'), 
+                new RecipeStepDto('', 'sprinkle surgar over pans, covering them evenly')
+            ])
+        ])];
+    }
     async getRecipeDetails(id) { }
     async updateRecipe(updateRecipeDto) { }
     async createRecipe(createRecipeDto) { }
