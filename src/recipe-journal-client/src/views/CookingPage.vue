82d8ab4154d8db4ebadf564d647d9266579942b4
sm_d3po <seanmarek@gmail.com>
2023-10-04 22:42:16 -0700

    some ui updates

--- src/recipe-journal-client/src/views/CookingPage.vue
+++ src/recipe-journal-client/src/views/CookingPage.vue
@@ -117,7 +117,7 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
 .article-container {
     max-width: 40rem;
     margin-left: auto;
@@ -191,5 +191,9 @@ button.slider-button {
 
 .section.section-completed {
      text-decoration: line-through; 
+}
+
+p {
+    white-space: pre-wrap;
 }
 </style>
--- src/recipe-journal-client/src/views/HomePage.vue
+++ src/recipe-journal-client/src/views/HomePage.vue
@@ -1,7 +1,17 @@
 <template>
     <div class="container">
         <h1>Welcome, it's time to cook!</h1>
-        <img alt="Vue logo" src="../assets/logo.png" />
+        <img style="float:right" alt="Vue logo" src="../assets/logo.png" />
+        <div style="">
+            Features Overview
+            <ul>
+                <li>Simple recipe layout with scaling calculator</li>
+                <li>Recipe CMS for easy input</li>
+                <li>Easy shopping list</li>
+                <li>Recipe journaling gathering notes for what happened and what to change in the future</li>
+                <li>Cooking Goals</li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -13,3 +23,9 @@ export default {
     methods: {},
 };
 </script>
+
+<style scoped>
+.container {
+    max-width: 40rem;
+}
+</style>
